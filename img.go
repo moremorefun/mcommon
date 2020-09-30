82b68d13458fdb0833316826c9492bdebc1f27b3
hao <unnamed.hao@gmail.com>
2020-09-30 10:44:52 +0800

    add font size get

--- img.go
+++ img.go
@@ -23,6 +23,39 @@ func getFontFamilyFont(fontPath string) (*truetype.Font, error) {
 	return f, err
 }
 
+// GetWriteSizeFont 获取指定字体的文字大小
+func GetWriteSizeFont(content string, fontSize float64, fontPath string) (int, int, error) {
+	c := freetype.NewContext()
+
+	c.SetDPI(72)
+	c.SetHinting(font.HintingFull)
+	// 设置文字颜色、字体、字大小
+	c.SetSrc(image.NewUniform(color.RGBA{R: 51, G: 51, B: 51, A: 254}))
+	c.SetFontSize(fontSize)
+	fontFam, err := getFontFamilyFont(fontPath)
+	if err != nil {
+		return 0, 0, err
+	}
+	c.SetFont(fontFam)
+
+	pt := freetype.Pt(0, 0)
+
+	newPt, err := c.DrawString(content, pt)
+	if err != nil {
+		return 0, 0, err
+	}
+	const shift, mask = 6, 1<<6 - 1
+	x := int32(newPt.X)
+	if x >= 0 {
+		return int(int32(x >> shift)), int(int32(x & mask)), nil
+	}
+	x = -x
+	if x >= 0 {
+		return -int(int32(x >> shift)), int(int32(x & mask)), nil
+	}
+	return 0, 0, nil
+}
+
 // WriteOnImageFont 根据指定字体写入图片
 func WriteOnImageFont(target *image.NRGBA, content string, fontSize float64, x int, y int, color color.RGBA, fontPath string) error {
 	c := freetype.NewContext()
