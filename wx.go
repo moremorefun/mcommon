c749461e9f5b91720035072825273f37ca96ca21
hao <unnamed.hao@gmail.com>
2020-10-27 12:14:46 +0800

    add wx app user info

--- wx.go
+++ wx.go
@@ -22,6 +22,29 @@ type WxJsCodeResp struct {
 	Errmsg     string `json:"errmsg"`
 }
 
+// WxAppCodeResp jscode回复
+type WxAppCodeResp struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int64  `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	Openid       string `json:"openid"`
+	Scope        string `json:"scope"`
+	ErrCode      int64  `json:"errcode"`
+}
+
+// WxAppUserInfoResp userinfo回复
+type WxAppUserInfoResp struct {
+	Openid     string `json:"openid"`
+	Nickname   string `json:"nickname"`
+	Sex        uint64 `json:"sex"`
+	Province   string `json:"province"`
+	City       string `json:"city"`
+	Country    string `json:"country"`
+	Headimgurl string `json:"headimgurl"`
+	Unionid    string `json:"unionid"`
+	ErrCode    int64  `json:"errcode"`
+}
+
 // WxJsCode js登录
 func WxJsCode(appID, appSecret, code string) (*WxJsCodeResp, error) {
 	retryCount := 0
@@ -52,6 +75,73 @@ GotoHttpRetry:
 	}
 	if 0 != apiResp.Errcode {
 		return nil, fmt.Errorf("wx jscode err: %s", apiResp.Errmsg)
+	}
+	return &apiResp, nil
+}
+
+// WxAppCode app登录
+func WxAppCode(appID, appSecret, code string) (*WxAppCodeResp, error) {
+	retryCount := 0
+GotoHttpRetry:
+	_, body, errs := gorequest.New().
+		Get("https://api.weixin.qq.com/sns/oauth2/access_token").
+		Query(gin.H{
+			"appid":      appID,
+			"secret":     appSecret,
+			"js_code":    code,
+			"grant_type": "authorization_code",
+		}).EndBytes()
+	if errs != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, errs[0]
+	}
+	var apiResp WxAppCodeResp
+	err := json.Unmarshal(body, &apiResp)
+	if err != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, fmt.Errorf("json err: %w %s", err, body)
+	}
+	if 0 != apiResp.ErrCode {
+		return nil, fmt.Errorf("wx jscode err: %d", apiResp.ErrCode)
+	}
+	return &apiResp, nil
+}
+
+// WxAppUserInfo 用户信息
+func WxAppUserInfo(accessToken, openID string) (*WxAppUserInfoResp, error) {
+	retryCount := 0
+GotoHttpRetry:
+	_, body, errs := gorequest.New().
+		Get("https://api.weixin.qq.com/sns/userinfo").
+		Query(gin.H{
+			"access_token":      accessToken,
+			"openid":     openID,
+			"lang":     "zh_CN",,
+		}).EndBytes()
+	if errs != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, errs[0]
+	}
+	var apiResp WxAppUserInfoResp
+	err := json.Unmarshal(body, &apiResp)
+	if err != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, fmt.Errorf("json err: %w %s", err, body)
+	}
+	if 0 != apiResp.ErrCode {
+		return nil, fmt.Errorf("wx jscode err: %d", apiResp.ErrCode)
 	}
 	return &apiResp, nil
 }
