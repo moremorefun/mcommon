819b6d42790fd447d46951898c50e99b1de71539
hao <unnamed.hao@gmail.com>
2021-01-22 17:44:06 +0800

    add wx pay v3

---
+++ wxpayv3.go
@@ -0,0 +1,149 @@
+package mcommon
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+type StWxPayV3EncryptResp struct {
+	Data []struct {
+		EffectiveTime      time.Time `json:"effective_time"`
+		EncryptCertificate struct {
+			Algorithm      string `json:"algorithm"`
+			AssociatedData string `json:"associated_data"`
+			Ciphertext     string `json:"ciphertext"`
+			Nonce          string `json:"nonce"`
+		} `json:"encrypt_certificate"`
+		ExpireTime time.Time `json:"expire_time"`
+		SerialNo   string    `json:"serial_no"`
+	} `json:"data"`
+}
+
+func RsaSign(signContent string, privateKey string, hash crypto.Hash) (string, error) {
+	shaNew := hash.New()
+	shaNew.Write([]byte(signContent))
+	hashed := shaNew.Sum(nil)
+	priKey, err := ParsePrivateKey(privateKey)
+	if err != nil {
+		return "", err
+	}
+	signature, err := rsa.SignPKCS1v15(rand.Reader, priKey, hash, hashed)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(signature), nil
+}
+
+func ParsePrivateKey(privateKey string) (*rsa.PrivateKey, error) {
+	// 2、解码私钥字节，生成加密对象
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		return nil, errors.New("私钥信息错误！")
+	}
+	// 3、解析DER编码的私钥，生成私钥对象
+	priKeyInc, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	priKey, ok := priKeyInc.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("error key type")
+	}
+	return priKey, nil
+}
+
+// WxPayV3Sign v3签名
+func WxPayV3Sign(mchid, keySerial, key string, req *gorequest.SuperAgent) (*gorequest.SuperAgent, error) {
+	timestamp := time.Now().Unix()
+	nonce := GetUUIDStr()
+
+	uri, err := url.Parse(req.Url)
+	if err != nil {
+		return nil, err
+	}
+
+	var bodyBytes []byte
+	if req.Method == "POST" {
+		request, err := req.MakeRequest()
+		if err != nil {
+			return nil, err
+		}
+		bodyReader, err := request.GetBody()
+		if err != nil {
+			return nil, err
+		}
+		bodyBytes, err = ioutil.ReadAll(bodyReader)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(req.Method)
+	buf.WriteString("\n")
+	buf.WriteString(uri.Path)
+	buf.WriteString("\n")
+	buf.WriteString(strconv.FormatInt(timestamp, 10))
+	buf.WriteString("\n")
+	buf.WriteString(nonce)
+	buf.WriteString("\n")
+	buf.Write(bodyBytes)
+	buf.WriteString("\n")
+	sign, err := RsaSign(buf.String(), key, crypto.SHA256)
+	if err != nil {
+		return nil, err
+	}
+
+	auth := fmt.Sprintf(
+		`WECHATPAY2-SHA256-RSA2048 mchid="%s",nonce_str="%s",signature="%s",timestamp="%d",serial_no="%s"`,
+		mchid,
+		nonce,
+		sign,
+		timestamp,
+		keySerial,
+	)
+
+	req = req.
+		Set("Authorization", auth).
+		Set("Accept", "application/json")
+	return req, nil
+}
+
+func WxPayV3Decrype(key string, cipherStr, nonce, associatedData string) (string, error) {
+	keyBytes := []byte(key)
+	nonceBytes := []byte(nonce)
+	associatedDataBytes := []byte(associatedData)
+	ciphertext, err := base64.StdEncoding.DecodeString(cipherStr)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	plaintext, err := aesgcm.Open(nil, nonceBytes, ciphertext, associatedDataBytes)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
+}
