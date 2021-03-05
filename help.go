fdcd7d6e4db7371468058b62ccbc899161a9731d
hao <unnamed.hao@gmail.com>
2021-03-05 10:26:45 +0800

    fix

--- help.go
+++ help.go
@@ -473,12 +473,11 @@ func GinMinTokenToUserIDRedisIgnore(tx DbExeAble, redisClient *redis.Client, get
 func GinCors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		// 可将将* 替换为指定的域名
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Max-Age", "3600")
 		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
 
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
